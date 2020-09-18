35bae02e69974de5cae0366c21f99a696df80575
David Lukes <dafydd.lukes@gmail.com>
2020-09-19 00:55:59 +0200

    wip

--- lua/luapraat.cpp
+++ lua/luapraat.cpp
@@ -3,6 +3,14 @@
 
 lua_State *L;
 
+// maybe there shouldn't be a global lua_State and this function should
+// instead return it to the caller, who would then pop the result off
+// the stack and/or do anything else they need to do with it? this would
+// be safer in case Praat Script does something fancy with multiple
+// threads; a global variable is obviously not threadsafe. on the flip
+// side, no state can then be shared between invocations (which is
+// probably fine?), the caller has to remember to free the state and
+// deal with Lua stack manipulation.
 char *luapraat_run() {
 		L = luaL_newstate();
 		luaL_openlibs(L);
