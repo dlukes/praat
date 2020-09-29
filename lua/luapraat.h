e7fb383e3aabe92bf4ac915e46c5c0da82cf7b92
David Lukes <dafydd.lukes@gmail.com>
2020-09-29 16:23:35 +0200

    wip: tweak headers and includes according to learncpp best practices

--- lua/luapraat.h
+++ lua/luapraat.h
@@ -1,10 +1,9 @@
-extern "C" {
-	#include <lua.h>
-	#include <lualib.h>
-	#include <lauxlib.h>
-}
+#ifndef _luapraat_h_
+#define _luapraat_h_
 
 #include "Interpreter.h"
 
 void luapraat_run_file(const char32 *script_path, Interpreter interpreter);
 void luapraat_run_chunk(const char32 *chunk, Interpreter interpreter);
+
+#endif
